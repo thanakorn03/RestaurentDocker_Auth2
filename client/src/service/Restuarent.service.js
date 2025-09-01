a3779f03e3e53b2ae87fcf21920787c0c5eb875f
thanakorn03 <iamok302@gmail.com>
2025-09-01 16:42:54 +0700

    fixservice

--- client/src/service/Restuarent.service.js
+++ client/src/service/Restuarent.service.js
@@ -1,11 +1,22 @@
-import api from "./api";
+import axios from "axios";
 
-const API_URL = "/restaurants";
+const API = axios.create({
+  baseURL: "https://restaurentdocker-auth2.onrender.com/api/v1",
+});
 
-export const createRestaurant = (data) => api.post(API_URL, data);
-export const getAllRestaurants = () => api.get(API_URL);
-export const getRestaurantById = (id) => api.get(`${API_URL}/${id}`);
-export const updateRestaurant = (id, data) => api.put(`${API_URL}/${id}`, data);
-export const deleteRestaurant = (id) => api.delete(`${API_URL}/${id}`);
+// เพิ่ม token อัตโนมัติถ้ามี
+API.interceptors.request.use(config => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (user?.accessToken) {
+    config.headers.Authorization = `Bearer ${user.accessToken}`;
+  }
+  return config;
+});
+
+export const createRestaurant = (data) => API.post("/restaurants", data);
+export const getAllRestaurants = () => API.get("/restaurants");
+export const getRestaurantById = (id) => API.get(`/restaurants/${id}`);
+export const updateRestaurant = (id, data) => API.put(`/restaurants/${id}`, data);
+export const deleteRestaurant = (id) => API.delete(`/restaurants/${id}`);
 
 export default { createRestaurant, getAllRestaurants, getRestaurantById, updateRestaurant, deleteRestaurant };
