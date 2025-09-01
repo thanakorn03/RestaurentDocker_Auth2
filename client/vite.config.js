0b5be2872bce4b8570a4ea527851be88b0378235
thanakorn03 <iamok302@gmail.com>
2025-09-01 15:40:48 +0700

    fixvercal

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
     port: 5173,
+    allowedHosts: [
+      'restaurentdocker-auth2-client.onrender.com', // ✅ เพิ่ม host ของคุณ
+      'localhost',
+      '127.0.0.1'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
