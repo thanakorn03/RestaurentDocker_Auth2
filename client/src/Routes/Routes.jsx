35ce0e0759a93b26b78aeeb79c7c4460d77b844d
thanakorn03 <iamok302@gmail.com>
2025-09-01 16:00:03 +0700

    fixcode4

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home";
 import AddRestaurant from "../Pages/AddRestaurant";
 import UpdateRestaurant from "../Pages/UpdateRestaurant";
-import Login from "../Pages/Login";
+import Login from "../Pages/login";
 import Register from "../Pages/Register"; // ✅ แก้ path
 
 const NotFound = () => (
@@ -16,8 +16,8 @@ const router = createBrowserRouter([
   { path: "/", element: <Home />, errorElement: <NotFound /> },
   { path: "/add-restaurant", element: <AddRestaurant />, errorElement: <NotFound /> },
   { path: "/update-restaurant/:id", element: <UpdateRestaurant />, errorElement: <NotFound /> },
-  { path: "/login", element: <Login />, errorElement: <NotFound /> },
-  { path: "/register", element: <Register />, errorElement: <NotFound /> },
+  { path: "/Login", element: <Login />, errorElement: <NotFound /> },
+  { path: "/Register", element: <Register />, errorElement: <NotFound /> },
   { path: "*", element: <NotFound /> },
 ]);
 
